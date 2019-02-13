bc571d9da180dec60b3041959bdfbc3e76a28ef1
hchung <hyunjun.chung@gmail.com>
2019-02-13 13:28:31 +0700

    Replace a tab character to two spaces

--- commit_helper.sh
+++ commit_helper.sh
@@ -5,6 +5,8 @@ if [ 0 = '${#filename}' ]; then
   exit 1
 fi
 
+cat $filename | sed -e 's/	/  /g' > $filename.tmp
+mv $filename.tmp $filename
 git diff $filename | grep '^[+-][ ]\{0,\}\*[^+]' | sed -e 's/\](/ /' | sed -e 's/)$//' | sed -e 's/\/$//' | pbcopy
 git add $filename
 git commit
