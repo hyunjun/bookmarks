ba64092c6aab975216730648dac07f3e8385b109
hchung <hyunjun.chung@gmail.com>
2019-03-05 14:08:09 +0700

    Can input multiple file names

--- commit_helper.sh
+++ commit_helper.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-filename=$1
+FILENAMES="$@"
 
-if [ 0 = '${#filename}' ]; then
+if [ 0 = '${#FILENAMES}' ]; then
   exit 1
 fi
 
-cat $filename | sed -e 's/	/  /g' > $filename.tmp
-mv $filename.tmp $filename
-git diff $filename | grep '^[+-][ ]\{0,\}\*[^+]' | sed -e 's/\](/ /' | sed -e 's/)$//' | sed -e 's/\/$//' | pbcopy
-git add $filename
+for FILENAME in $FILENAMES
+do
+  echo $f
+  cat $FILENAME | sed -e 's/	/  /g' > $FILENAME.tmp
+  mv $FILENAME.tmp $FILENAME
+done
+git diff $FILENAMES | grep '^[+-][ ]\{0,\}\*[^+]' | sed -e 's/\](/ /' | sed -e 's/)$//' | sed -e 's/\/$//' | pbcopy
+git add $FILENAMES
 git commit
