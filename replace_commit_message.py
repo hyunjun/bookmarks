ed83793e295e03779530c68fc6c9d26c75df937f
hyunjun.chung <agapelover4u@yahoo.co.kr>
2020-01-11 10:33:38 +0700

    Modify regex to remove '(NOT YET)'

--- replace_commit_message.py
+++ replace_commit_message.py
@@ -3,12 +3,12 @@ import sys
 
 
 #PATTERN_BASIC = re.compile(r'^[ \t]*\+\* \[(\*\*){0,1}(?P<title>.+)(\*\*){0,1}\]\((?P<addr>.+)\)')
-PATTERN_BASIC = re.compile(r'^[ \t]*\+[ \t]*\* \[(?P<title>.+)\]\((?P<addr>.+)\)')
+PATTERN_BASIC = re.compile(r'^[ \t]*\+[ \t]*\* (\(NOT YET\) )?\[(?P<title>.+)\]\((?P<addr>.+)\)')
 PATTERN_URL_INSIDE_BRACKET = re.compile(r'http(.)+(?P<ending_bracket>(/){0,1}\])')
 
 
 def removeUnnecessaryPart(line):
-    line = line.replace('[**', '[').replace('**]', ']').replace('/]', ']').replace('/)', ')')
+    line = line.replace('[**', '[').replace('**]', ']').replace('/]', ']').replace('/)', ')')#.replace('(NOT YET)', '')
     m = PATTERN_URL_INSIDE_BRACKET.search(line)
     if m:
         line = re.sub(r'/? \]\(', '](', line)
@@ -27,6 +27,7 @@ data = [('+* [산드로 만쿠소, “소프트웨어 장인정신이란…”](
         ('  +* [산드로 만쿠소, “소프트웨어 장인정신이란…” http://someurl/ ](http://www.bloter.net/archives/251535/)', '산드로 만쿠소, “소프트웨어 장인정신이란…” http://someurl http://www.bloter.net/archives/251535'),
         ('  +    * [산드로 만쿠소, “소프트웨어 장인정신이란…” http://someurl ](http://www.bloter.net/archives/251535/)', '산드로 만쿠소, “소프트웨어 장인정신이란…” http://someurl http://www.bloter.net/archives/251535'),
         ('  +    * [산드로 만쿠소, “소프트웨어 장인정신이란…” http://someurl/ ](http://www.bloter.net/archives/251535/)', '산드로 만쿠소, “소프트웨어 장인정신이란…” http://someurl http://www.bloter.net/archives/251535'),
+        ('  +* (NOT YET) [산드로 만쿠소, “소프트웨어 장인정신이란…” http://someurl/ ](http://www.bloter.net/archives/251535/)', '산드로 만쿠소, “소프트웨어 장인정신이란…” http://someurl http://www.bloter.net/archives/251535'),
         ]
 
 
